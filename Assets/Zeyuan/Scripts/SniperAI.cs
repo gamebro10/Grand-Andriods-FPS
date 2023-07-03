984d9564e8d9e21dc0a8d7bbf3bbc251fe8997fa
rooot16 <948142627@qq.com>
2023-07-03 15:41:41 -0400

    1. Added two more types of enemies 2. Downloaded an outline shader from UnityStore

---
+++ Assets/Zeyuan/Scripts/SniperAI.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SniperAI : EnemyBase
+{
+    [SerializeField] float shootRange;
+    [SerializeField] float aimTime;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform firePos;
+    [SerializeField] LineRenderer laser;
+
+    bool showLaser;
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        if (currentTarget == Player)
+        {
+            InCombat();
+            if (showLaser)
+            {
+                ShowLaser();
+            }
+        }
+        else
+        {
+            OutCombat();
+        }
+    }
+
+    void Attack()
+    {
+        Debug.DrawRay(firePos.position, Player.transform.position - firePos.position);
+        if (!isShooting)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(firePos.position, Player.transform.position - firePos.position, out hit, shootRange, layerMask))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    StartCoroutine(IAttack());
+                }
+            }
+        }
+    }
+
+    IEnumerator IAttack()
+    {
+        isShooting = true;
+        showLaser = true;
+        laser.gameObject.SetActive(true);
+        yield return new WaitForSeconds(aimTime);
+        Instantiate(bullet, firePos.transform.position, Quaternion.LookRotation(Player.transform.position - firePos.position));
+        showLaser = false;
+        laser.gameObject.SetActive(false);
+        yield return new WaitForSeconds(attackCD);
+        isShooting = false;
+    }
+
+    void ShowLaser()
+    {
+        laser.SetPosition(0, firePos.transform.position);
+        laser.SetPosition(1, Player.transform.position);
+    }
+
+    void InCombat()
+    {
+        FaceToPlayer();
+        if (Vector3.Distance(transform.position, Player.transform.position) > shootRange)
+        {
+            MoveToPlayer();
+        }
+        else
+        {
+            Attack();
+        }
+    }
+
+    void OutCombat()
+    {
+        if (isPlayerInRange && CanSeePlayer())
+        {
+            currentTarget = Player;
+        }
+        else
+        {
+            StartCoroutine(Iroam());
+        }
+    }
+}
---
+++ Assets/Zeyuan/Scripts/SoldierAI.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SoldierAI : EnemyBase
+{
+    [SerializeField] float shootRange;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform firePos;
+
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        if (currentTarget == Player)
+        {
+            InCombat();
+        }
+        else
+        {
+            OutCombat();
+        }
+    }
+
+    void Attack()
+    {
+        Debug.DrawRay(firePos.position, Player.transform.position - firePos.position);
+        if (!isShooting)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(firePos.position, Player.transform.position - firePos.position, out hit, shootRange, layerMask)) 
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    StartCoroutine(IAttack());
+                }
+            }
+        }
+    }
+
+    IEnumerator IAttack()
+    {
+        isShooting = true;
+        int randomAttackCount = UnityEngine.Random.Range(2, 5);
+
+        for (int i = 0; i < randomAttackCount; i++)
+        {
+            Instantiate(bullet, firePos.transform.position, Quaternion.LookRotation(Player.transform.position - firePos.position));
+            yield return new WaitForSeconds(attackRate);
+        }
+        yield return new WaitForSeconds(attackCD);
+        isShooting = false;
+    }
+
+    protected override IEnumerator HitAndRun()
+    {
+        if (!destinationChosen)
+        {
+            float tempSpeed = agent.speed;
+            agent.speed = 2;
+            agent.updateRotation = false;
+            destinationChosen = true;
+            agent.stoppingDistance = 0;
+
+            float randomAmount = Random.Range(3f, 5f);
+            Vector3 moveDirection = Random.Range(0, 2) == 0 ? -transform.right * randomAmount : transform.right * randomAmount;
+            Vector3 randomPos = transform.position + moveDirection;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, randomAmount, 1);
+
+            agent.SetDestination(hit.position);
+
+            yield return new WaitUntil(() => { return agent.remainingDistance - agent.stoppingDistance <= 0.05; });
+            agent.speed = tempSpeed;
+            agent.updateRotation = true;
+            destinationChosen = false;
+        }
+    }
+
+    void InCombat()
+    {
+        FaceToPlayer();
+        if (Vector3.Distance(transform.position, Player.transform.position) > shootRange)
+        {
+            MoveToPlayer();
+        }
+        else
+        {
+            if (!isShooting)
+            {
+                int shouldStayOrMove = Random.Range(0, 10);
+                if (shouldStayOrMove < 7)//80% change to hit and run, 20% to stay and shoot
+                {
+                    StartCoroutine(HitAndRun());
+                    Attack();
+                }
+                else
+                {
+                    Attack();
+                }
+            }
+        }
+    }
+
+    void OutCombat()
+    {
+        if (isPlayerInRange && CanSeePlayer())
+        {
+            currentTarget = Player;
+        }
+        else
+        {
+            StartCoroutine(Iroam());
+        }
+    }
+}
