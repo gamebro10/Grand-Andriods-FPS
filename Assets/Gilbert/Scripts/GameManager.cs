1adde5e0d21490f04bc47eb6a69c1bde67ff8caa
GP0521 <gpemberton0521@gmail.com>
2023-07-04 01:28:45 -0400

    Basic GameManager and IDamage Scripts created

---
+++ Assets/Gilbert/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [Header("----- Player Stuff -----")]
+    public GameObject player;
+   // public PlayerController playerScript;
+    public GameObject playerSpawnPos;
+
+    [Header("----- UI Stuff -----")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+    public GameObject titleMenu;
+    public GameObject optionsMenu;
+    public GameObject levelSelect;
+    public TextMeshProUGUI enemiesRemainingText;
+    public Image playerHPBar;
+    public GameObject playerFlashDamageScreen;
+
+    int enemiesRemaining;
+    bool isPaused;
+    float timescaleOrig;
+
+    void Awake()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
