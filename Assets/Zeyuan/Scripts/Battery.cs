b9b48d997c1d600b3c2f3ea8ff0ca146b75fb254
rooot16 <948142627@qq.com>
2023-07-19 23:09:06 -0400

    Battery

---
+++ Assets/Zeyuan/Scripts/Battery.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : Interactable
+{
+    float floatAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.Instance.isPaused)
+        {
+            float pos = Mathf.Sin(floatAmount) * Mathf.PI / 180 * .2f;
+            floatAmount += Time.deltaTime * 4;
+            transform.position += new Vector3(0, pos, 0);
+            transform.Rotate(new Vector3(0, Time.deltaTime, 0) * 40, Space.World);
+
+            CheckInteraction();
+            if (Input.GetKeyDown(KeyCode.E) && canInteract)
+            {
+                OnInteract();
+            }
+        }
+    }
+
+    protected override void OnInteract()
+    {
+        GameManager.Instance.statePaused();
+        GameManager.Instance.activeMenu = GameManager.Instance.winMenu;
+        GameManager.Instance.activeMenu.SetActive(true);
+        Destroy(gameObject);
+    }
+}
