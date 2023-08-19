86f1eebbd7804412807def1a3e3b600143cb365d
Ajp783 <Tony_7-8-3@outlook.com>
2023-08-19 19:09:14 -0400

    Audio fixes and Gun fixes

--- Assets/Anthony/Anthony Assets/Scripts/Gunstats.cs
+++ Assets/Anthony/Anthony Assets/Scripts/Gunstats.cs
@@ -7,11 +7,7 @@ using UnityEngine;
 
 public class Gunstats : ScriptableObject
 {
-    public int shootdistance;
-    public float shotRate;
-    public int shootdmg;
-    public GameObject model;
 
-
-    //public ParticleSystem hiteffect;
+   
+   
 }
