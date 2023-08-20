7d6e8cbc6c11d493ec6795cbe5bc667332f13f4d
Masonicle <123963705+Masonicle@users.noreply.github.com>
2023-08-20 18:18:04 -0400

    Rework Stomp and Dash

--- Assets/Mason/Scripts/NewDash.cs
+++ Assets/Mason/Scripts/NewDash.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class NewDash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float dashForce = 10f;
+    public float dashDuration = 0.2f;
+    public Transform cameraTransform; 
+
+    private Rigidbody rb;
+    private bool isDashing = false;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            Dash();
+        }
+    }
+
+    private void Dash()
+    {
+        Vector3 horizontalVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        Vector3 cameraForward = cameraTransform.forward;
+
+       
+        Vector3 dashDirection = (horizontalVelocity + cameraForward).normalized;
+
+        rb.AddForce(dashDirection * dashForce, ForceMode.VelocityChange);
+
+      
+        float rotationAngle = Mathf.Atan2(dashDirection.x, dashDirection.z) * Mathf.Rad2Deg;
+
+      
+        Quaternion targetRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+
+     
+        rb.MoveRotation(targetRotation);
+
+        isDashing = true;
+        Invoke(nameof(EndDash), dashDuration);
+    }
+
+    private void EndDash()
+    {
+        isDashing = false;
     }
 }
+
