409c761a34f0e03ebefc2d15235d207dbdf378b8
chosen <jaibhatia1989@gmail.com>
2023-08-20 21:52:22 -0400

    tutorial level bounce pad

---
+++ Assets/SoundEffectBounce.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectBounce : MonoBehaviour
+{
+    [SerializeField] AudioSource bounceEffect;
+    [SerializeField] AudioClip bounceClip;
+
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") )
+        {
+            bounceEffect.PlayOneShot(bounceClip);
+        }
+    }
+}
